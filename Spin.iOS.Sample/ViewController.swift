55288bfd6e47b9f12a486596fe44650207c3a66d
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2019-09-01 19:50:37 -0400

    Initial Commit

---
+++ Spin.iOS.Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spin.iOS.Sample
+//
+//  Created by Thibault Wittemberg on 2019-09-01.
+//  Copyright © 2019 Spinners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
